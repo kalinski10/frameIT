a9cc607ce5e09d06d3c6150040d42b8318ec433c
kalin balabnov <kalin_dude10@live.com>
2019-11-03 21:15:03 +0000

    fixed image interaction

--- frameIT2/frameIT2/ViewController.swift
+++ frameIT2/frameIT2/ViewController.swift
@@ -10,41 +10,73 @@ import UIKit
 import AVFoundation
 import Photos
 
-class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate, UIGestureRecognizerDelegate {
 
     @IBOutlet weak var creationFrame: UIView!
     @IBOutlet weak var creationImageView: UIImageView!
-    @IBOutlet weak var startOverButton: UIButton!
     @IBOutlet weak var colorLbale: UILabel!
-    @IBOutlet weak var colorsContainer: UIButton!
-    @IBOutlet weak var shareButton: UIButton!
+    @IBOutlet weak var colorsContainer: UIView!
     
     var creation = Creation.init()
     var localImages = [UIImage].init()
     let defaults = UserDefaults.standard
     var colorSwatches = [ColorSwatch].init()
     
+    let colorUserDefaultsKey = "ColorIndex"
+    var savedColorSwatchIndex: Int {
+        get {
+            let savedIndex = defaults.value(forKey: colorUserDefaultsKey)
+            if savedIndex == nil {
+                defaults.set(colorSwatches.count - 1, forKey: colorUserDefaultsKey)
+            }
+            return defaults.integer(forKey: colorUserDefaultsKey)
+        }
+        set {
+            if newValue >= 0 && newValue < colorSwatches.count {
+                defaults.set(newValue, forKey: colorUserDefaultsKey)
+            }
+        }
+    }
+    
+    @objc func changeImageView(_ sender: UITapGestureRecognizer) {
+        displayImagePickingOptions()
+        print("tap")
+    }
+    
+    @objc func moveImageView(_ sender: UIPanGestureRecognizer) {
+        print("moving")
+    }
+    
+    @objc func rotateImageView(_ sender: UIRotationGestureRecognizer) {
+        print("rotating")
+    }
+    
+    @objc func scaleImageVIew(_ sender: UIPinchGestureRecognizer) {
+        print("scaling")
+    }
+    
     @IBAction func startOverButton(_ sender: Any) {
         creation.reset(colorSwatch: colorSwatches[savedColorSwatchIndex])
         creationImageView.image = creation.image
         creationFrame.backgroundColor = creation.colorSwatch.color
         colorLbale.text = creation.colorSwatch.caption
+        print("over")
     }
+    
     @IBAction func share(_ sender: Any) {
         if let index = colorSwatches.firstIndex(where: {$0.caption == creation.colorSwatch.caption})
         {
             savedColorSwatchIndex = index
         }
-        displayImagePickingOptions()
     }
     @IBAction func applyColor(_ sender: UIButton) {
+        //print("Applying color")
         if let index = colorsContainer.subviews.firstIndex(of: sender) {
             creation.colorSwatch = colorSwatches[index]
             creationFrame.backgroundColor = creation.colorSwatch.color
             colorLbale.text = creation.colorSwatch.caption
         }
     }
-    
     
     func displayImagePickingOptions(){
         let alertController = UIAlertController(title: "Choose image", message: nil, preferredStyle: .actionSheet)
@@ -66,7 +98,7 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         alertController.addAction(libraryAction)
         alertController.addAction(randomAction)
         alertController.addAction(cancelAction)
-        
+
         present(alertController, animated: true){
             
         }
@@ -91,6 +123,8 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
                 self.presentImagePicker(sourceType: sourceType)
             case .denied, .restricted:
                 self.troubleAlert(message: noPermissionMessage)
+            @unknown default:
+                print("error")
             }
         } else {
             troubleAlert(message: "Sincere apologies, it looks like we can't access your camera at this time")
@@ -99,9 +133,7 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     
     func displayLibrary() {
         let sourceType = UIImagePickerController.SourceType.photoLibrary
-        
         if UIImagePickerController.isSourceTypeAvailable(sourceType) {
-    
             let status = PHPhotoLibrary.authorizationStatus()
             let noPermissionMessage = "Looks like FrameIT doesn't have access to your photos :( please use settings app on your device to permit FrameIT accessing your library"
             
@@ -118,6 +150,8 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
                 self.presentImagePicker(sourceType: sourceType)
             case .restricted, .denied:
                 self.troubleAlert(message: noPermissionMessage)
+            @unknown default:
+                print("error")
             }
         } else {
             troubleAlert(message: "Sincere apologies, it looks like we can't access your photo library at this time")
@@ -137,21 +171,19 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     }
     
     func troubleAlert(message: String?) {
-        
         let alertController = UIAlertController(title: "Oops...", message: "Sincere apologies, it looks like we can't access your photo libarary at this time", preferredStyle: .alert)
         let OKAction = UIAlertAction(title: "Got it", style: .cancel)
         alertController.addAction(OKAction)
         present(alertController, animated: true)
-        
     }
     
     func processPicked(image: UIImage?){
         if let newImage = image {
             creation.image = newImage
             creationImageView.image = creation.image
-            
         }
     }
+    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         picker.dismiss(animated: true, completion: nil)
         let newImage = info [UIImagePickerController.InfoKey.originalImage] as? UIImage
@@ -168,11 +200,8 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
             }
         }
     }
-   
     func randomImage() -> UIImage? {
-        
         let currentImage = creationImageView.image
-        
         if localImages.count > 0 {
             while true {
                 let randomIndex = Int.random(in: 0..<localImages.count)
@@ -187,11 +216,12 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     
     func collectColors() {
         colorSwatches = [
+            ColorSwatch.init(caption: "Sunshine", color: UIColor.init(red: 242/255, green: 197/255, blue: 0/255, alpha: 1)),
+            ColorSwatch.init(caption: "Candy", color: UIColor.init(red: 221/255, green: 51/255, blue: 27/255, alpha: 1)),
             ColorSwatch.init(caption: "Ocean", color: UIColor.init(red: 44/255, green: 151/255, blue: 222/255, alpha: 1)),
             ColorSwatch.init(caption: "Shamrock", color: UIColor.init(red: 28/255, green: 188/255, blue: 100/255, alpha: 1)),
-            ColorSwatch.init(caption: "Candy", color: UIColor.init(red: 221/255, green: 51/255, blue: 27/255, alpha: 1)),
-            ColorSwatch.init(caption: "Violet", color: UIColor.init(red: 136/255, green: 20/255, blue: 221/255, alpha: 1)),
-            ColorSwatch.init(caption: "Sunshine", color: UIColor.init(red: 242/255, green: 197/255, blue: 0/255, alpha: 1))]
+            ColorSwatch.init(caption: "Violet", color: UIColor.init(red: 136/255, green: 20/255, blue: 221/255, alpha: 1))
+            ]
     // taking the colours that we added so they match with our colors
         if colorSwatches.count == colorsContainer.subviews.count {
             for i in 0 ..< colorSwatches.count {
@@ -200,20 +230,21 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         }
     }
     
-    let colorUserDefaultsKey = "ColorIndex"
-    var savedColorSwatchIndex: Int {
-        get {
-            let savedIndex = defaults.value(forKey: colorUserDefaultsKey)
-            if savedIndex == nil {
-                defaults.set(colorSwatches.count - 1, forKey: colorUserDefaultsKey)
-            }
-            return defaults.integer(forKey: colorUserDefaultsKey)
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer)
+            -> Bool {
+        // simultaneous gesture recognition will only be supported for creationImageView
+        if gestureRecognizer.view != creationImageView {
+            return false
         }
-        set {
-            if newValue >= 0 && newValue < colorSwatches.count {
-                defaults.set(newValue, forKey: colorUserDefaultsKey)
-            }
+        // neither of the recognized gestures should not be tap gesture
+        if gestureRecognizer is UITapGestureRecognizer
+            || otherGestureRecognizer is UITapGestureRecognizer
+            || gestureRecognizer is UIPanGestureRecognizer
+            || otherGestureRecognizer is UIPanGestureRecognizer {
+            return false
         }
+                
+        return true
     }
     
     func configure() {
@@ -223,6 +254,18 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         creationImageView.image = creation.image
         creationFrame.backgroundColor = creation.colorSwatch.color
         colorLbale.text = creation.colorSwatch.caption
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(changeImageView(_:)))
+        let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(moveImageView(_:)))
+        let rotationGestureRecognizer = UIRotationGestureRecognizer(target: self, action: #selector(rotateImageView(_:)))
+        let pinchGestureRecognizer = UIPinchGestureRecognizer(target: self, action: #selector(scaleImageVIew(_:)))
+        creationImageView.addGestureRecognizer(tapGestureRecognizer)
+        creationImageView.addGestureRecognizer(panGestureRecognizer)
+        creationImageView.addGestureRecognizer(pinchGestureRecognizer)
+        creationImageView.addGestureRecognizer(rotationGestureRecognizer)
+        tapGestureRecognizer.delegate = self
+        panGestureRecognizer.delegate = self
+        pinchGestureRecognizer.delegate = self
+        rotationGestureRecognizer.delegate = self
     }
     
     override func viewDidLoad() {
